4d266cb59bc1cc77d562de49c74c8bd09dc5e791
betaniasan <betaniasantos.san@gmail.com>
2023-10-06 21:15:18 +0300

    10 e 11 completo

--- src/main/java/a042807/Game.java
+++ src/main/java/a042807/Game.java
@@ -1,76 +1,48 @@
 package a042807;
 
 import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
-import javax.swing.*;
 import java.io.IOException;
 
 public class Game {
     Terminal terminal;
+    Arena arena;
     Screen screen;
     TerminalSize terminalSize ;
-    private  int x = 10;
-    private int y = 10;
-
-    private boolean quit = false;
-
-
-
     public Game() throws IOException {
-            terminal = new DefaultTerminalFactory().createTerminal();
-            screen = new TerminalScreen(terminal);
-            terminalSize = new TerminalSize(40, 20);
-            screen.setCursorPosition(null); // we don't need a cursor
-            screen.startScreen(); // screens must be started
-            screen.doResizeIfNecessary(); // resize screen if necessary
+        arena = new Arena(75,40);
+        terminal = new DefaultTerminalFactory().createTerminal();
+        screen = new TerminalScreen(terminal);
+        terminalSize = new TerminalSize(40, 20);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory()
+                .setInitialTerminalSize(terminalSize);
+        terminal = terminalFactory.createTerminal();
+        screen.setCursorPosition(null); // we don't need a cursor
+        screen.startScreen(); // screens must be started
+        screen.doResizeIfNecessary(); // resize screen if necessary
 
-            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory()
-                    .setInitialTerminalSize(terminalSize);
-            terminal = terminalFactory.createTerminal();
     }
-
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
-
+        arena.draw(screen.newTextGraphics(), screen);
         screen.refresh();
     }
     public void run() throws IOException {
-        while(!quit) {
+        while (arena.isRunning()) {
             draw();
             com.googlecode.lanterna.input.KeyStroke key = screen.readInput();
             processKey(key);
         }
+
+    }
+
+    private void processKey(com.googlecode.lanterna.input.KeyStroke key ) throws IOException {
+        arena.processKey(key, screen);
     }
 
 
-    private void processKey(KeyStroke key) throws IOException {
-        switch (key.getKeyType()) {
-            case ArrowUp:
-                y--;
-                break;
-            case ArrowRight:
-                x++;
-                break;
-            case ArrowDown:
-                y++;
-                break;
-            case ArrowLeft:
-                x--;
-                break;
-            case Character:
-                if (key.getCharacter() == 'q') {
-                    screen.close();
-                    return;
-                }
-            case EOF:
-                quit = true;
-        }
-    }
 }
--- src/main/java/a042807/Hero.java
+++ src/main/java/a042807/Hero.java
@@ -1,27 +1,46 @@
 package a042807;
 
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+
 public class Hero {
-    private int x;
-    private  int y;
 
-    public Hero(int x, int y) {
-        this.x = x;
-        this.y = y;
+
+
+    private Position position;
+    public Hero(int x, int y ){
+        position = new Position(x, y);
+    }
+    public Position moveUp() {
+        return new Position(position.getX(), position.getY() - 1);
+    }
+    public Position moveDown() {
+        return new Position(position.getX(), position.getY() + 1);
+    }
+    public Position moveLeft() {
+        return new Position(position.getX() -1, position.getY());
+    }
+    public Position moveRight() {
+        return new Position(position.getX() +1, position.getY());
+    }
+    public void Draw(TextGraphics graphics, Screen screen){
+        screen.setCharacter(position.getX(), position.getY(), new TextCharacter('X', TextColor.ANSI.DEFAULT, TextColor.ANSI.DEFAULT));
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FFFF33"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(position.getX() * 2, position.getY() * 2), "\\/");
+        graphics.putString(new TerminalPosition(position.getX() * 2, position.getY() * 2 + 1), "/\\");
     }
 
-    public int getX(){
-        return x;
-    }
 
-    public int getY() {
-        return y;
+    public void setPosition(Position position) {
+        this.position = position;
     }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public void setX(int x) {
-        this.x = x;
+    public Position getPosition() {
+        return position;
     }
 }
+
