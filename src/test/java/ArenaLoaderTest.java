15dbe63d472c336fd7a86f39192049afdaa8b523
Betânia Santos <131388308+betaniasan@users.noreply.github.com>
2023-10-25 15:41:11 +0100

    Testes ArenaTest.java, todos testes OK, definição dos getters e setter para conseguir implementar todos os testes correctamente

--- src/test/java/ArenaLoaderTest.java
+++ src/test/java/ArenaLoaderTest.java
@@ -1,2 +1,28 @@
+import Main.Arena;
+import Misc.ArenaLoader;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ArenaLoaderTest {
+
+    @Test
+    public void testLoadArenaFromFile() throws IOException {
+        Arena arena = ArenaLoader.loadArenaFromFile("maps/map1.txt");
+
+        // Verifique se a arena foi carregada corretamente
+        assertNotNull(arena);
+        assertEquals(80, arena.getWidth());
+        assertEquals(24, arena.getHeight());
+
+        // Verifique se o herói, as paredes, as moedas e os monstros foram carregados corretamente
+        assertNotNull(arena.getHero());
+        assertFalse(arena.getWalls().isEmpty());
+        assertFalse(arena.getCoins().isEmpty());
+        assertFalse(arena.getMonsters().isEmpty());
+    }
+
+    // Adicione mais testes conforme necessário para verificar se todos os elementos foram carregados corretamente
 }
